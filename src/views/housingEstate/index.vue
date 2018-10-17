21dc7c467e430614146d662d4f607ab3d82feb5d
gc <812474076@qq.com>
2018-10-17 09:15:50 +0800

    新建第三页页面

---
+++ src/views/housingEstate/index.vue
@@ -0,0 +1,43 @@
+ <!--
+  @author guochen
+  @date 2018/10/16 上午10.40
+  @desc 住宅小区房型，行情介绍（第三张图）
+-->
+<template>
+    <div>
+      <!-- 住宅图片展示 -->
+      <div>
+        <img src="../../assets/guessLike.png" class="housingImg"/>
+      </div>
+      <!-- 住宅图片展示 -->
+      <div class="housingType">
+        <div class="buttonType">在售</div>
+        <button>住宅</button>
+      </div>
+      <div>
+        <housing-estate-nav></housing-estate-nav>
+      </div>
+    </div>
+    
+</template>
+
+<script>
+import './index.scss';
+import HousingEstateNnav from './nav/nav';
+
+export default {
+  name: 'housingEstate',
+  data() {
+    return {
+    };
+  },
+  components: {
+    'housing-estate-nav': HousingEstateNnav
+  },
+  created() {},
+  mounted() {},
+  methods: {
+  },
+};
+
+</script>
---
+++ src/views/housingEstate/nav/nav.vue
@@ -0,0 +1,66 @@
+ <!--
+  @author guochen
+  @date 2018/10/16 上午10.40
+  @desc 住宅小区房型，行情介绍（第三张图）
+-->
+
+<template>
+  <div class="housing-estate-nav">
+    <ul class="nav-ul">
+      <li class="nav-li">
+
+      </li>
+      <li class="nav-li">
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import './nav.scss';
+
+export default {
+  name: 'housing-estate-nav',
+  data() {
+    return {
+      firstNav: [
+        {
+          name: '住宅',
+          icon: 'icon-zhuzhai',
+          link: '',
+        },
+        {
+          name: '商铺',
+          icon: 'icon-shangpu',
+          link: '',
+        },
+        {
+          name: '公寓',
+          icon: 'icon-gongyu',
+          link: '',
+        },
+      ],
+      sedondNav: [
+        {
+          name: '写字楼',
+          icon: 'icon-xiezilou',
+          link: '',
+        },
+        {
+          name: '优质房源',
+          icon: 'icon-youzhifangyuan',
+          link: '',
+        },
+      ],
+    };
+  },
+  components: {},
+  created() {
+
+
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
