ce95969c806f406467a3a8b455e8615c2773c554
Leon1Zhu <zl937154020@163.com>
2018-10-20 17:37:21 +0800

    搜索条优化

--- src/views/searchPage/searchContent/transitionPage/transitionPage.vue
+++ src/views/searchPage/searchContent/transitionPage/transitionPage.vue
@@ -16,14 +16,14 @@
             <li v-for="(item, index) in rightContent" >
               <span :class="{'active' : index === childIndex}" @click="searchData(index, item)">{{item}}</span>
             </li>
-
-            <li v-if="searchItem === 'price'" class="price-selected-content">
-              <span class="selected-price">10000-20000</span>
-              <div class="price-silder">
-                <mu-slider class="demo-slider" v-model="value1" :max="50000"></mu-slider>
-                <mu-slider class="demo-slider" v-model="value2" :min="50001" :max="100000"></mu-slider>
-              </div>
-            </li>
+            <!--<li v-if="searchItem === 'price'" class="price-selected-content">-->
+              <!--<span class="selected-price" v-if="!iswx">{{value1}}-{{value2}}</span>-->
+              <!--<span class="selected-price" v-else>不限</span>-->
+              <!--<div class="price-silder">-->
+                <!--<mu-slider class="demo-slider" v-model="value1" :max="50000"></mu-slider>-->
+                <!--<mu-slider class="demo-slider" v-model="value2" :min="50001" :max="100000"></mu-slider>-->
+              <!--</div>-->
+            <!--</li>-->
           </ul>
         </div>
       </div>
@@ -41,6 +41,7 @@ export default {
     return {
       value1:0,
       value2:100000,
+      iswx: false,
       parIndex: 0,
       childIndex: null,
       searchInfo: {
@@ -147,6 +148,13 @@ export default {
     searchData(index, item){
       this.childIndex = index;
       this.$emit('searchData', this.parIndex, index, item)
+      // if (this.searchItem === 'price') {
+      //   if (item !== '不限') {
+      //     const arr = item.split('-');
+      //     this.value1 = arr[0];
+      //     this.value2 = arr[1];
+      //   }
+      // }
     },
   },
 };
