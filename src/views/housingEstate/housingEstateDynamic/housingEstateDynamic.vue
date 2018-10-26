65ea7e63c330927a089d88f8816bc1070fda4d24
gc <812474076@qq.com>
2018-10-26 21:36:43 +0800

    修改第三页即户型页面

---
+++ src/views/housingEstate/housingEstateDynamic/housingEstateDynamic.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="house-type-info">
+    <div class="house-type-title">
+        <span class="name">楼盘动态</span>
+        <span class="temp-span"></span>
+        <span class="view-all">全部动态</span>
+    </div>
+    <div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "./housingEstateDynamic.scss";
+import { swiper, swiperSlide } from 'vue-awesome-swiper';
+
+export default {
+  name: "house-type",
+  data() {
+    return {
+      swiperOptionValue: {
+        spaceBetween: 15,
+      },
+    };
+  },
+  computed: {
+    swiper() {
+      return this.$refs.qualitySwiper.swiper
+    }
+  },
+  components: {
+    swiper,
+    swiperSlide
+  },
+  created() {},
+  mounted() {},
+  methods: {}
+};
+</script>
